d9efc74bb41c56784672fc7ef487cf98387bba4d
im23b-hubere <hubere@bzz.ch>
2025-03-25 11:31:27 +0100

    safe push

--- API/app.py
+++ API/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import pymysql
 
 app = Flask(__name__)
-
+CORS(app)
 def get_db_connection():
     connection = pymysql.connect(
         host='localhost',
